750f61bef072f112a9bbc4338d276e2ab56fec6b
Haddee Ann <haddeeann@gmail.com>
2022-01-01 15:49:40 -0500

    added a word count to the paragraph

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,34 @@
-console.log('hello client side javascript');
+// console.log('hello client side javascript');
+
+class WordCount extends HTMLParagraphElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // Element functionality written in here
+      this.attachShadow({ mode: 'open' });
+      let elText = '';
+      let wordCountText = '';
+      // make the spans
+      let wrapper = document.createElement('span');
+      let wordCountEl = document.createElement('span');
+
+      // get text from doc
+      let datatext = this.getAttribute('data-text');
+
+      elText = this.innerHTML;
+
+      let elTextCount = elText.trim().split(' ').length;
+
+      // add count to word count text
+      wordCountText = datatext + `... the paragraph word count is ${elTextCount}`;
+
+      // add text to elements
+      wrapper.innerHTML = elText;
+      wordCountEl.innerHTML = wordCountText
+      this.shadowRoot.append(wrapper);
+      this.shadowRoot.append(wordCountEl);
+    }
+}
+
+customElements.define('word-count', WordCount, { extends: 'p' });
